1b73fc118bc30b596075c4cd49b804afda563820
wehong <thingworx@163.com>
2017-01-13 00:30:43 +0800

    init windchill 11 project

---
+++ src/com/lenovo/plm/ebg/arbortext/ShipmentTypeEnum.java
@@ -0,0 +1,10 @@
+package com.lenovo.plm.ebg.arbortext;
+import wt.fc.DynamicEnumType;
+import com.ptc.windchill.annotations.FrozenSerializable;
+import com.ptc.windchill.annotations.metadata.*;
+
+@GenAsEnumeratedType(superClass=DynamicEnumType.class, supportedAPI=SupportedAPI.PUBLIC)
+@FrozenSerializable
+public class ShipmentTypeEnum extends _ShipmentTypeEnum {
+	private static final long serialVersionUID = 1;
+}
